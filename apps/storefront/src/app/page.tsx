952ed2b232449dee9c62c5a569db23b79c58704b
tue-truonga-otsv <tue.truonga@one-line.com>
2023-10-02 09:52:22 +0700

    feat: init commit

---
+++ apps/storefront/src/app/page.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export const metadata = {
+  title: 'Store | Kitchen Sink',
+};
+
+export default function Store() {
+  return (
+    <div className="container">
+      <h1 className="title">
+        Store <br />
+        <span>Kitchen Sink</span>
+      </h1>
+      <Link href={'/dev'}>DEV page</Link>
+    </div>
+  );
+}
---
+++ packages/eslint-config-custom/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ['next', 'turbo', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve('next/babel')],
+    },
+  },
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+  },
+};
